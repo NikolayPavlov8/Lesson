12f7a3f9b4532db68eaa7ea692747f2a2a9b7a0b
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-08 14:02:16 +0300

    code2

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
+// Кортеж
+let http404Error = (404, "Not Found")
+
+// Задается все переменные
+let (statusCode, statusMessage) = http404Error
+print("Код статуса: \(statusCode)")
+print("Сообщение: \(statusMessage)")
+
+// задается одна переменная
+let (justStatusCode, _) = http404Error
+print("Только код статуса: \(justStatusCode)")
+
+// вывод кортежа другим способом, по номеру
+print("Код статуса: \(http404Error.0)")
+print("Сообщение: \(http404Error.1)")
+
+// даем имена кортежам
+let http200Status = (statusCode1: 200, descrition: "OK")
+print("Код статуса: \(http200Status.statusCode1)")
+print("Сообщение: \(http200Status.descrition)")
 

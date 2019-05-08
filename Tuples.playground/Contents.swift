4af1353aa3ba0875c6f089a1e696dae4b714b557
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-08 14:25:53 +0300

    code3

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -1,6 +1,19 @@
 import UIKit
 
 // Кортеж
+// let имяКонстанты = (значение_1, значение_2, ...)
+// var имяПеременной = (значение_1, значение_2, ...)
+
+// объявляем кортеж
+let myProgramStatus = (200, "In Work", true)
+// записываем значения кортежа в переменные
+var (statusCode2, statusText, statusConnect) = myProgramStatus
+// выводим информацию
+print("Код ответа - \(statusCode2)")
+print("Текст ответа - \(statusText)")
+print("Связь с сервером - \(statusConnect)")
+
+
 let http404Error = (404, "Not Found")
 
 // Задается все переменные
